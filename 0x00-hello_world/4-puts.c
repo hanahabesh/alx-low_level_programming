df41a02cf6a6a6811eca4cde8ac323250b5a53bd
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-15 02:08:42 -0700

    my first c programming on vim

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * this program is the first c proram print using puts 
  */
 int main(void)
+//some decrition
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
