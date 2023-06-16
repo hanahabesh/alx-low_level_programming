c2ce7d6cd9f136d3f99dcc11502b799b3e2e3f2f
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-16 08:30:27 -0700

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+	char a;
+	
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
