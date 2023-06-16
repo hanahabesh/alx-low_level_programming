49cc12a3f287ac33578e2fa584df5a38f698f079
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-16 05:17:28 -0700

    hg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fputet("and that piece of art is useful\" - Dora Kor
-par, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - DorKorpar, 2015-10-19\n",
+			 stdout);
 	return (1);
 }
