8ad3b700c47a54b4697efbfee9cda60068860a27
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-16 07:08:48 -0700

    lower alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		if (lower != 'e' && lower != 'q')
+			putchar(lower);
+	}
+	putchar('\n');
+	return (0);
+}
