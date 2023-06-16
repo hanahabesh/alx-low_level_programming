b5f25268789095e5b381b05ed6cb53361ac7f173
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-16 05:41:29 -0700

    display the lower case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower_alphabet = 'a';
+
+	while (lower_alphabet <= 'z')
+	{
+		putchar(lower_alphabet);
+	
+		lower_alphabet++;
+	}
+
+	putchar('\n');
+	return (0);
+}
