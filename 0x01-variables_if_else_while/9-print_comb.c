ec16ba0f98a6e1e8e7e7ed7be15a94308f0f9839
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-16 09:53:08 -0700

    numbet

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	int i;
+	i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		
+		if (i != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
