a2a94a84bd5d682f3d705881a49f7a7fecbea9cc
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-17 09:43:53 -0700

    trial

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,22 +8,22 @@
 int main(void)
 {
 	int i, j, k;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
 			for (k = j + 1; k <= '9'; k++)
 			{
-				if (i != j && j != k && k != i && i < j && i <
- k && j < k)
+				if (i != j && j != k && k != i && i < j && i
+< k && j < k)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
 					{
-						if (i == '7' && j == '8' && k 
-== '9')
+						if (i == '7' && j == '8' && 
+k == '9')
 						{
 							putchar('\n');
 						}
