163d29e0cb91347ff2f94261baa0c781943acc44
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-20 12:32:44 -0700

    tired

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: return 0
+*/
+int main(void)
+{
+	int x;
+	long j = 1, k = 2;
+
+	for (x = 0; x < 50; x++)
+	{
+		if (x == 0)
+			printf("%ld", j);
+		else if (x == 1)
+			printf(", %ld", k);
+		else
+		{
+			k = k + j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
