7bf8b72a50e906d046a38a2c3ab256519a241a7a
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-22 05:56:17 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Fizz Buzz
+ * Return: no return value
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf(" Fizz ");
+		else if (i % 5 == 0)
+			printf(" Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf(" FizzBuzz ");
+		else
+			printf(" %d ", i);
+	}
+	putchar('\n');
+	return (0);
+}
