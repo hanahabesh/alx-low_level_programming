435e007eebad67f1ecbd1bd288eea3f751b933cc
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-22 05:28:54 -0700

    square of the hashtags

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - hashtag print
+ * @size: size of square
+ * Return: no return value
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
