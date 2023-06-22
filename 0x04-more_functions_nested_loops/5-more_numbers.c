fec45710cc2275c4a50fcefe042bf51b1780f227
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-21 23:30:32 -0700

    multiple number repeated

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - write numbers repeatedly
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 14; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
+
