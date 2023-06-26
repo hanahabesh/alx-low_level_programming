9ba3836fc3c2fd350d80fcc9834b52401f1df6b9
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-25 23:30:14 -0700

    swip the numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two integers
+ * @a: number one
+ * @b: number two
+ * return: Return void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
