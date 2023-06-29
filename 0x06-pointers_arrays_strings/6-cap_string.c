e20a02b5768b72f317a71b212ee3cafcfe16dc9f
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-29 06:26:48 -0700

    capitalized all word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,23 +8,23 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
-			str[i - 1] == 44 ||
-			str[i - 1] == 59 ||
-			str[i - 1] == 46 ||
-			str[i - 1] == 33 ||
-			str[i - 1] == 63 ||
-			str[i - 1] == 34 ||
-			str[i - 1] == 123 ||
-			str[i - 1] == 125 ||
-			str[i - 1] == 40 ||
-			str[i - 1] == 41 ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
 			i == 0)
 				str[i] -= 32;
 			i++;
