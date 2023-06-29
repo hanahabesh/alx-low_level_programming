f0346eca053a16be4274012e63628f7464d5e0bb
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-29 07:57:43 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,15 +7,18 @@
 char *rot13(char *a)
 {
 	int i, j;
-	char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdfghijklmnopqrstuvwxyz";
-	char str2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdfghijklm";
+	char str1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char str2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
 			if (a[i] == str1[j])
+			{
 				a[i] = str2[j];
+				break;
+			}
 		}
 	}
 	return (a);
