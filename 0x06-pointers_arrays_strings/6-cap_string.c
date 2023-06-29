71b1454706d6c4a65e81a94ae7e8195e2a2364b1
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-29 06:17:42 -0700

    capitalized all word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,33 @@
 #include "main.h"
 /**
- * _string - function to capitalize every single word in string
+ * cap_string - function to capitalize every single word in string
  * @str: the string
  * Return: return capitalized word
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	if (a[i] <= 'a' && 
+	while (str[i] != '\0')
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == 44 ||
+			str[i - 1] == 59 ||
+			str[i - 1] == 46 ||
+			str[i - 1] == 33 ||
+			str[i - 1] == 63 ||
+			str[i - 1] == 34 ||
+			str[i - 1] == 123 ||
+			str[i - 1] == 125 ||
+			str[i - 1] == 40 ||
+			str[i - 1] == 41 ||
+			i == 0)
+				str[i] -= 32;
+			i++;
+	}
+	return (str);
+}
