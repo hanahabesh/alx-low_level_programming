9a3546bd5de9b50aa10873031eb25bf0ac903fd4
Hana Getachew Addis <hanihabesh@gmail.com>
2023-06-29 05:37:44 -0700

    copy the string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,14 +8,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0';)
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	for (i = 0; i < n;)
+	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _string - function to capitalize every single word in string
+ * @str: the string
+ * Return: return capitalized word
+ */
+char *cap_string(char *)
+{
+	int i;
+
+	if (a[i] <= 'a' && 
