b7f862b88aa03a8b9dc99b30af0e8be2723285f5
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-03 09:14:22 -0700

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - double pointer
+ * @s: string one
+ * @to: the other string
+ * Return: return void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
