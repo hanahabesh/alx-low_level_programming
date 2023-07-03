6ad7b469173f0ad78811be0a03871a48aa9b5b84
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-03 09:38:54 -0700

    string compare function

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int i;
+	int j = n;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < j; i++)
+	{
 		src[i] = dest[i];
+		n--;
+	}
 	return (dest);
 }
