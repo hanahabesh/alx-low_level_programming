03343a91b2ce4f854abb2624b3c70deb0d605f02
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-10 08:05:02 -0700

    duplicate the string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - deplicate the string
+ * @str: the original string
+ * Return: return the duplicated
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 1; str[i];)
+		i++;
+	dup = malloc((i * sizeof(char)) + 1);
+	if (dup == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		dup[j] = str[j];
+	}
+	dup[j] = '\0';
+	return (dup);
+}
