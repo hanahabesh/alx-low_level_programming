410b02723568ae631f890abf4001949a0fa86803
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-10 12:12:03 -0700

    free that space

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - free the allocated space
+ * @grid: the 2D array
+ * @height: length of array
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int n;
+
+	for (n = 0; n <= height; n++)
+		free(grid[n]);
+	free(grid);
+}
