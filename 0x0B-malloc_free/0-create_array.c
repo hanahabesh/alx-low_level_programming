aaf1670e51f90f2ac19d26a7ab2fc0a45ff78ea5
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-10 05:32:32 -0700

    malloc for char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	char *s;
 	unsigned int i = 0;
 
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	if (size == 0)
