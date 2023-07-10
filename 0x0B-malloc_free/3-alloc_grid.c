2ea48100910b232606c4871d5f92adba17a2bb82
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-10 11:35:04 -0700

    2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,6 @@ int **alloc_grid(int width, int height)
 	S = malloc(height * sizeof(int *));
 	if (S == NULL)
 	{
-		free(S);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -25,9 +24,7 @@ int **alloc_grid(int width, int height)
 		if (S[i] == NULL)
 		{
 			for (j = 0; j >= 0; j--)
-			{
 				free(S[j]);
-			}
 			free(S);
 			return (NULL);
 		}
