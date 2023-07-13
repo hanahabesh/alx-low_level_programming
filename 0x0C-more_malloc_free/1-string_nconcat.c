17a50b56cc7ccc9d04b9766f9f1633e0703dd3b2
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-13 06:53:42 -0700

    strings concatnate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,6 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k, size;
-	unsigned int l1, l2;
 	char *S;
 
 	if (s1 == NULL)
@@ -18,33 +17,31 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0';)
 	{
 		i++;
-		l1 = i;
 	}
 	if (s2 == NULL)
 		s2 = "";
 	for (j = 0; s2[j] != '\0';)
 	{
 		j++;
-		l2 = j;
 	}
-	if (n >= l2)
-		size = l1 + l2;
+	if (n >= j)
+		size = i + j;
 	else
-		size = l1 + n;
+		size = i + n;
 	S = malloc(size * sizeof(char) + 1);
 	if (S == NULL)
 		return (NULL);
 	j = 0;
 	for (k = 0; k < size; k++)
 	{
-		if (k <= l1)
+		if (k <= i)
 			S[k] = s1[k];
-		if (k >= l1)
+		if (k >= i)
 		{
 			S[k] = s2[j];
 			j++;
 		}
 	}
-	S[size + 1] = '\0';
+	S[k] = '\0';
 	return (S);
 }
