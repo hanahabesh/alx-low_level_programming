c1c582b7952a4bfca028c119a0adaba70d02aca5
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-19 08:05:49 -0700

    operation

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef _CALC_H_
-#define CALC_H_
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct op - Struct op
  *
@@ -8,9 +8,10 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
