6b41a071f133710a38f609aa2df9b39c2fccbf5d
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-19 08:43:58 -0700

    done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print the opcode
+ * @argc: count og an arguments
+ * @argv: arguments
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	int i, byt;
+	unsigned char opcode;
+	int (*p)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Erro\n");
+		exit(1);
+	}
+	byt = atoi(argv[1]);
+	if (byt < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	for (i = 0; i < byt; i++)
+	{
+		opcode = *(unsigned char *)p;
+		printf("%.2x", opcode);
+		if (i == (byt - 1))
+			continue;
+		printf(" ");
+		p++;
+	}
+	printf("\n");
+	return (0);
+}
+
