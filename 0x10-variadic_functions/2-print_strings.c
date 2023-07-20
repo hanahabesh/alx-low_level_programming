14b55f76d9973bd8054f621dda1690ae2b80683d
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-20 10:21:36 -0700

    list

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - print the strings that passed through arguments
+ * @separator: the commas
+ * @n: the number of arguments
+ * Return: return nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	char *S;
+	unsigned int i;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		S = va_arg(list, char *);
+		if (S == NULL)
+			printf("(nil)");
+		printf("%s", S);
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(list);
+}
