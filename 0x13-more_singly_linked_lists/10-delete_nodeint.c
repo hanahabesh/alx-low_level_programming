78dde8fc98fb2d6959ff2b09c54c243e2db31346
Hana Getachew Addis <hanihabesh@gmail.com>
2023-07-31 22:11:47 -0700

    delet node at certain position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,12 +19,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-	if (index == 1)
-	{
-		*head = current->next;
-		free(current);
-		return (1);
-	}
 	while (i < (index - 1))
 	{
 		if (current->next == NULL)
