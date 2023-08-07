aab2d3e1eac5e13cc71fdd7c44d86dc2207508a4
Hana Getachew Addis <hanihabesh@gmail.com>
2023-08-07 08:26:30 -0700

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,9 @@ int create_file(const char *filename, char *text_content)
 			i++;
 	}
 	d = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	x = write(d == -1 || x == -1)
-		return (-);
-	closed(d);
+	x = write(d, text_content, i);
+	if(d == -1 || x == -1)
+		return (-1);
+	close(d);
 	return (1);
 }
