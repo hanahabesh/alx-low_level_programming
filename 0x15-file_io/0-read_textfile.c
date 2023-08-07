4690f52672ec7bbdbf7781d699c4faf9f875fde4
Hana Getachew Addis <hanihabesh@gmail.com>
2023-08-07 08:04:37 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - a function to read a file
+ * @filename: the intended file name
+ * @letters: the proposed sie in byte
+ * Return: return the value or zero
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t x;
+	ssize_t y;
+	ssize_t z;
+	char *descriptor;
+
+	x = open(filename, O_RDONLY);
+	if (x == -1)
+		return (0);
+	descriptor = malloc(sizeof(char) * letters);
+	z = read(x, descriptor, letters);
+	y = writes(STDOUT_FILENO, descriptor, z);
+	free(descriptor);
+	close(x);
+	return (y);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+File handling or input and output in C programming language
